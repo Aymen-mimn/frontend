e7ff09150997f82e67b0371818d09df685aecb19
Aymen-mimn <93287578+Aymen-mimn@users.noreply.github.com>
2026-05-05 22:41:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let img = document.querySelector('.main');
+let container = document.querySelector('.container');
+
+
+
+function change(phone){
+  img.src = phone ;
+}
+
+function switchC(x){
+  container.style.background = x;
+}
